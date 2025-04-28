ca406d8407d52d9ae45e9afa64964e6268294351
SicilianoFranco21 <sicilianofranco21@gmail.com>
2025-04-28 18:42:24 -0300

    Create test files and declare TestMage and TestWarrior classes

---
+++ tests/test_mage.py
@@ -0,0 +1,10 @@
+from mage import Mage
+from unittest import TestCase, main
+
+
+class TestMage(TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
---
+++ tests/test_warrior.py
@@ -0,0 +1,10 @@
+from warrior import Warrior
+from unittest import TestCase, main
+
+
+class TestWarrior(TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
