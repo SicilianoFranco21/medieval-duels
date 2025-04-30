d38819cd5e828ae87092b297ad65feaf2467f1c6
SicilianoFranco21 <sicilianofranco21@gmail.com>
2025-04-30 17:58:28 -0300

    review pretty_test_result.py

---
+++ tests/utils/pretty_test_result.py
@@ -0,0 +1,20 @@
+from unittest import TestCase, TextTestResult
+
+
+class PrettyTestResult(TextTestResult):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.successes: list[TestCase] = []
+    
+    def addSuccess(self, test: TestCase) -> None:
+        super().addSuccess(test)
+        self.successes.append(test)
+        self.stream.writeln(f"✅ {test._testMethodName:<44} - {test.shortDescription()}")
+    
+    def addFailure(self, test: TestCase, err) -> None:
+        super().addFailure(test, err)
+        self.stream.writeln(f"❌ {test._testMethodName:<44} - {test.shortDescription()}")
+
+    def addError(self, test: TestCase, err) -> None:
+        super().addError(test, err)
+        self.stream.writeln(f"⚠️ {test._testMethodName:<44} - {test.shortDescription()}")
