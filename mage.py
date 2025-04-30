9e72f1745f2d5b25fc9628ce482e431c653f7d1e
SicilianoFranco21 <sicilianofranco21@gmail.com>
2025-04-30 13:54:53 -0300

    Refactor mage.py

--- mage.py
+++ mage.py
@@ -2,6 +2,8 @@ from character import Character
 
 
 class Mage(Character):
+    _MANA_COST: int = 25
+    
     def __init__(
         self, 
         name: str, 
@@ -15,30 +17,44 @@ class Mage(Character):
         self._mana = mana
     
     @property
+    def ENERGY_COST(self) -> int:
+        return 10
+
+    @property
     def mana(self) -> int:
         return self._mana
     
     @mana.setter
     def mana(self, new_mana: int) -> None:
         self._mana = new_mana
+
+    def _calculate_magical_damage(self, buff: int) -> int:
+        buff = max(1, buff)
+        magical_damage: int = (self.attack*buff)
+        return magical_damage
+        
+    def _deduct_mana(self) -> None:
+        self.mana = max(0, self.mana - self._MANA_COST)
     
-    def cast_magic_attack(self, target: Character) -> None:
-        energy_cost: int = 5
-        mana_cost: int = 25
-        if not target.is_alive():
-            print(f"{target.name} is dead. (x_x)")
-            return
-        if self.energy < energy_cost:
-            print(f"Not enough energy to perform the attack")
-            return
-        if self.mana < mana_cost:
-            print(f"Not enough mana to perform the attack")
-            return
-        self.energy = max(0, self.energy - energy_cost)
-        self.mana = max(0, self.mana - mana_cost)
-        magical_damage: int = (self.attack)*10
-        target.receive_damage(magical_damage)
-        print(f"{self.name} cast a spell on {target.name}")
+    def _check_magical_resources(self) -> int:
+        if self.mana < self._MANA_COST:
+            return self._ActionStatus.INSUFFICIENT_MANA
+        
+        self._deduct_mana()
+        return self._ActionStatus.SUCCESS
+
+    def cast_magic_attack(self, target: Character, buff: int) -> int:
+        resource_status: int = self._check_resources()
+        if resource_status != self._ActionStatus.SUCCESS:
+            return resource_status
+        
+        magical_resource_status: int = self._check_magical_resources()
+        if magical_resource_status != self._ActionStatus.SUCCESS:
+            return magical_resource_status
+        
+        magical_damage: int = self._calculate_magical_damage(buff)
+        inflicted_damage: int = target._receive_damage(magical_damage)
+        return inflicted_damage
     
     def __repr__(self) -> str:
         return (f"Mage(name={self.name!r}, mana={self.mana!r}, health={self.health!r}, "
