50d60d1ab37d22e309fb93342a02433bbbac2f43
Franco S. <Sicilianofranco21@gmail.com>
2025-05-01 18:00:22 -0300

    Delete tests/test_battleLogger.py

--- tests/test_battleLogger.py
+++
@@ -1,9 +0,0 @@
-from battleLogger import BattleLogger
-from unittest import TestCase
-from unittest.mock import patch
-from io import StringIO
-
-
-class TestBattleLogger(TestCase):
-    def setUp(self) -> None:
-        pass
