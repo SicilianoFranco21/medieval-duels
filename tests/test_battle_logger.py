f0a9a69ee8c10d525f23c885e86a3fed86ac182f
SicilianoFranco21 <sicilianofranco21@gmail.com>
2025-05-01 18:01:32 -0300

    Declare TestBattleLogger class

---
+++ tests/test_battle_logger.py
@@ -0,0 +1,9 @@
+from battle_logger import BattleLogger
+from unittest import TestCase
+from unittest.mock import patch
+from io import StringIO
+
+
+class TestBattleLogger(TestCase):
+    def setUp(self) -> None:
+        pass
