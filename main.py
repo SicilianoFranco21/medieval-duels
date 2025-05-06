d898b956458e362c3b9138ded5e2078364dc25c4
SicilianoFranco21 <sicilianofranco21@gmail.com>
2025-05-06 15:36:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,10 @@ from core.mage import Mage
 from core.warrior import Warrior
 from controller.battle_controller import BattleController
 from session.battle_session import BattleSession
+from session.main_session import MainSession
 
 
 def main() -> None:
-    player: Warrior = Warrior("HERO")
-    cpu: Mage = Mage("VILLIAN")
-
-    controller: BattleController = BattleController(player, cpu)
-    session: BattleSession = BattleSession(controller)
-
-    winner: Character = session.run()
-    print(f"The winner is: {winner.name}")
-
+    session: MainSession = MainSession()
+    session.run()
 main()
